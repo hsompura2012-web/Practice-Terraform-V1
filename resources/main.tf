de4e95cc4586869d55b2e7d6b7a4e1202e41f15b
Harshit <hsompura2012@gmail.com>
2025-12-01 18:01:20 +0530

    Update

--- resources/main.tf
+++ resources/main.tf
@@ -1,8 +1,17 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [ "sg-07306e019683163e7" ]
 
   tags = {
-    Name = "Frontend"
+    Name = "frontend"
   }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z055196614WSVQYU4VYMA"
+  name    = "frontend-dev"
+  type    = "A"
+  ttl     = 50
+  records = [aws_instance.frontend.private_ip]
 }
